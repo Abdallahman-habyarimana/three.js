d8ed1415189d242d6a96fa3f6c96aff53f32353c
Abdallahman Habyarimana <habdallah005@gmail.com>
2025-12-03 11:31:50 -0500

    adjusting css and installing orbitcontrols

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 
 const cubeGeometry = new THREE.BoxGeometry(1,1,1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red"})
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red", cursor: "pointer"})
 
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
 scene.add(cubeMesh)
 
 // initialize the camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30);
+const camera = new THREE.PerspectiveCamera(
+  35, 
+  window.innerWidth / window.innerHeight, 
+  0.2, 
+  65);
 
 camera.position.z = 5
 
@@ -21,6 +26,9 @@ const canvas = document.querySelector("canvas.threejs")
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 })
+
+// instatiate the controls
+const controls = new OrbitControls(camera, canvas);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera)
